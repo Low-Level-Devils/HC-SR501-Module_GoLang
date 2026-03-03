3589da4111a852ad12ce54400c77d060890589a7
Josh Walker <walkerje1992@gmail.com>
2026-03-02 18:41:15 -0700

    Implement initial logic

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module sr501-mod
 
 go 1.25.0
 
-require periph.io/x/conn/v3 v3.7.2
+require (
+	github.com/fatih/color v1.18.0
+	periph.io/x/conn/v3 v3.7.2
+	periph.io/x/host/v3 v3.8.5
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+)
--- sr501.go
+++ sr501.go
@@ -1,11 +1,49 @@
 package sr501_mod
 
 import (
+	"fmt"
+
+	"github.com/fatih/color"
 	"periph.io/x/conn/v3/gpio"
+	"periph.io/x/conn/v3/gpio/gpioreg"
+	"periph.io/x/host/v3"
 )
 
 type Sensor struct {
 	pinName string
 	pin     gpio.PinIO
 	Events  chan bool
+}
+
+func NewSensor(pinName string) (*Sensor, error) {
+	if _, err := host.Init(); err != nil {
+		color.Red("Failed to get hardware registers for HC-SR501")
+
+		return nil, err
+	}
+
+	pin := gpioreg.ByName(pinName)
+
+	if pin == nil {
+		color.Red("Failed to get pin %s for HC-SR501", pinName)
+		return nil, fmt.Errorf("Failed to get pin")
+	}
+
+	if err := pin.In(gpio.PullDown, gpio.BothEdges); err != nil {
+		return nil, err
+	}
+
+	return &Sensor{
+		pinName: pinName,
+		pin:     pin,
+		Events:  make(chan bool),
+	}, nil
+}
+
+func (sensor *Sensor) Watch() {
+	for {
+		if sensor.pin.WaitForEdge(-1) {
+			sensor.Events <- (sensor.pin.Read() == gpio.High)
+		}
+	}
 }
